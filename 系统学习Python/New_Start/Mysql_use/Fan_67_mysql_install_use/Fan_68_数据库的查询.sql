c7c650c6a846b14245b46e24ddfe7a04ac9d0e2d
zcx <648510978@qq.com>
2019-10-09 11:25:33 +0800

    聚合,分组,分页 已经完成

--- 系统学习Python/New_Start/Mysql_use/Fan_67_mysql_install_use/Fan_68_数据库的查询.sql
+++ 系统学习Python/New_Start/Mysql_use/Fan_67_mysql_install_use/Fan_68_数据库的查询.sql
@@ -70,3 +70,70 @@
     -- 例子 这个是多个字段联合排序,如果没有多个字段,就写一个就好了
     -- 查询 身高大于180 的男性 按照身高 从小到大排序,如果相同 按照id 从大到小排序
     select * from student where age>180 order by height asc, id desc ;
+
+-- 聚合函数
+    -- 总数
+    -- count
+    -- 查询一共有多少条记录
+    select count(*) from student;
+    -- 查询一共有多少条男性
+    select count(*) from student where gender=1;
+    -- 查询男性有多少人
+    select count(*) as 男性人数 from student where gender=1;
+
+    -- 最大值
+    -- max
+    -- 查询女性中最大的年龄
+    select max(age) as 女性最大年龄 from student where gender=2;
+    -- 查询女性中最大的升高
+    select max(height) as 女性中最高的身高 from student where gender=2;
+
+    -- 最小值
+    -- min
+    -- 查询女性中最小的年龄
+    select min(age) as 女性最大年龄 from student where gender=2;
+    -- 查询女性中最小的身高
+    select min(height) as 女性中最高的身高 from student where gender=2;
+
+    -- 求和
+    -- sum
+    -- 查询所有女性年龄总和
+    select sum(age) as 女性最大年龄 from student where gender=2;
+    -- 查询所有女性身高总和
+    select sum(height) as 女性身高总和 from student where gender=2;
+
+    -- 平均数
+    -- avg
+    -- 查询所有男性年龄的平均值
+    select avg(age) as 女性年龄平均值 from student where gender=2;
+    select sum(age)/count(*) as 女性年龄平均值 from student where gender=2;
+
+    -- 四舍五入
+    -- round(123.33,1) 1表示保留一位小数
+    -- 计算所有学生平均年龄 保留两位小数
+    select round(avg(age), 2) as 所有学生平均年龄 from student;
+
+
+-- 分组
+    -- group by
+    -- 按照性别分组,查询所有的性别
+    select gender as 性别 from student group by gender;
+
+    -- 计算每组性别中的人数
+    select gender as 性别, count(*) as 人数 from student group by gender;
+
+    -- 计算男性的人数
+    select gender as 性别, count(*) as 人数 from student where gender=1 group by gender;
+
+    -- group_concat(字段1,字段2,"字符串1",...)
+    -- 查询同一种性别,及其姓名列表  还有 人数 升序排列
+    select gender as 性别, group_concat(name," ") as 姓名列表, count(*) as 人数 from student group by gender order by count(*) asc ;
+
+
+-- 分页
+    -- limit start, count ; start 表示开始位置(必须是具体的数字,不能是表达式) count 表示每一页的页数
+    -- 查询所有的女性记录 每一页显示 3条数据
+    select * from student where gender=1 limit 0, 3;
+
+    -- 查询所有的女性记录 按照身高由高到低排序 每一页显示 3条数据(有一个记录点,limit 使用一定要在最后)
+    select * from student where gender=1 order by height desc limit 0, 3;
